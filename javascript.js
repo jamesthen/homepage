7bf2a188ea81e94530892f94d46fc89583061cf4
jamesthen <18248196+jamesthen@users.noreply.github.com>
2024-05-06 19:21:29 -0400

    operators

--- javascript.js
+++ javascript.js
@@ -13,15 +13,6 @@ myNumber = Number(myNumber) + 3;
 
 document.getElementById("demo").innerHTML = (typeof myInt + typeof myFloat + twoDecimalPlaces + "<br>" + myNumber);
 
-
-const num1 = 10;
-const num2 = 50;
-9 * num1; //90
-num1 ** 3; //1000
-num2 / num1; // 5
-5 + 10 * 3; // 45
-(num2 % 9) * num1; // 50
-num2 + num1 / 8 + 2; // 6
-
-
-document.getElementById("maths").innerHTML = (num2 + num1 / 8 + 2);
+let num1 = 4;
+num1++;
+num1;
