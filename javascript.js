88580b774527b83b335bb85dfb138e51f23673ea
jamesthen <18248196+jamesthen@users.noreply.github.com>
2024-05-06 19:13:35 -0400

    num operators added

--- javascript.js
+++ javascript.js
@@ -12,3 +12,13 @@ let myNumber = "74";
 myNumber = Number(myNumber) + 3;
 
 document.getElementById("demo").innerHTML = (typeof myInt + typeof myFloat + twoDecimalPlaces + "<br>" + myNumber);
+
+
+const num1 = 10;
+const num2 = 50;
+9 * num1; //90
+num1 ** 3; //1000
+num2 / num1; // 5
+5 + 10 * 3; // 45
+(num2 % 9) * num1; // 50
+num2 + num1 / 8 + 2; // 6
