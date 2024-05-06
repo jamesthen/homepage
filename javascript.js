22ecc4b1a809e15b82053b52b68a3dd058665f50
jamesthen <18248196+jamesthen@users.noreply.github.com>
2024-05-06 19:06:10 -0400

    syntax fix

--- javascript.js
+++ javascript.js
@@ -6,9 +6,9 @@ console.log(myInt)
 console.log(myFloat)
 
 let lotsOfDecimal = 1.903248983429843209843098423;
-let twoDecimalPLaces = lotsOfDecimal.toFixed(2);
+let twoDecimalPlaces = lotsOfDecimal.toFixed(2);
 
 let myNumber = "74";
 myNumber = Number(myNUmber) + 3;
 
-document.getElementById("demo").innerHTML = (typeof myInt + typeof myFloat + twoDecimalPLaces <br>myNumber);
+document.getElementById("demo").innerHTML = (typeof myInt + typeof myFloat + twoDecimalPlaces + "<br>" + myNumber);
